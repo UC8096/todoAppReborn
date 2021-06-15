df86eeb21c4f26951ce7465e902d3c00cde5b47c
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2021-06-16 00:03:45 +0700

    add SplashScreen page

---
+++ lib/ui/pages/spashscreen.dart
@@ -0,0 +1,13 @@
+part of 'pages.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      child: Text("SpalshScreen"),
+    );
+  }
+}
