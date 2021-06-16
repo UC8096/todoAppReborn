7a9b1b5df1329bf3a2faa934aa1a995433fb7fb2
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2021-06-16 23:35:08 +0700

    add app themes

---
+++ lib/ui/themes/apptheme.dart
@@ -0,0 +1,10 @@
+part of 'themes.dart';
+
+Color bgSplashScreen = Color.fromRGBO(227, 233, 255, 1);
+Color bgTopSplashScreen = Color.fromRGBO(237, 241, 255, 1);
+Color blueLogoBox = Color.fromRGBO(102, 127, 255, 1);
+Color blueLogoBoxShadow = Color.fromRGBO(51, 85, 255, 0.3);
+
+TextStyle poppins = GoogleFonts.poppins(fontWeight: FontWeight.w500);
+TextStyle blackPoppins = poppins.copyWith(color: Colors.black);
+TextStyle whitePoppins = poppins.copyWith(color: Colors.white);
