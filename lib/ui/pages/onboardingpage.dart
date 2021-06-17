1db8c19087311ef50baf96cff53c49a658496340
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2021-06-17 17:11:05 +0700

    add onboardingpage ui with the style

--- lib/ui/pages/onboardingpage.dart
+++ lib/ui/pages/onboardingpage.dart
@@ -1,1 +1,134 @@
 part of 'pages.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: bgBlue,
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                child: Image.asset(
+                  'assets/left_path.png',
+                  height: Get.height * 0.3,
+                ),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Image.asset(
+                  'assets/right_path.png',
+                  height: Get.height * 0.25,
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/ilustrasi_start.png',
+                      width: Get.width * 0.8,
+                    ),
+                    SizedBox(height: 35),
+                    Container(
+                      margin: EdgeInsets.only(left: 25),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  'Catat ',
+                                  style: blueSharpPoppins.copyWith(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                Text(
+                                  'Kegiatan Kamu ',
+                                  style: blackPoppins.copyWith(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              'Supaya ngga kelupaan sama aktifitas kamu.\nYuk! Catat segala aktifitas kamu',
+                              style: blackPalePoppins.copyWith(
+                                fontSize: 13,
+                              ),
+                            ),
+                          ]),
+                    ),
+                    SizedBox(height: Get.height * 0.15),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          padding: EdgeInsets.zero,
+                        ),
+                        onPressed: () {
+                          Get.toNamed('/homepage');
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xff667FFF),
+                                Color(0xffFF9999),
+                              ],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                          ),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Get Started',
+                                  style: whitePoppins.copyWith(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.keyboard_arrow_right_sharp,
+                                  color: Colors.white,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 35)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todoappreborn/ui/themes/themes.dart';
 
 part 'homepage.dart';
 part 'onboardingpage.dart';
